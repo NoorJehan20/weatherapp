c0c1ae746101bafb056392a8345a208b83ca4db2
Noor Jehan <137771389+NoorJehan20@users.noreply.github.com>
2024-02-18 11:31:35 +0500

    general

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { WiCloud } from "react-icons/wi";
+// import { WiCloud } from "react-icons/wi";
 import { useEffect, useState } from 'react';
 
 function App() {
 const [result, setresult] = useState({})
+const [city, setcity] = useState("")
 const API ={
   key: "1315e207be3242acf3af559b696c94ac",
   url: "https://api.openweathermap.org/data/2.5/weather"
+} 
+const handleSearch = () => {
+  console.log(city, "city")
+  fetch(`${API.url}?q=${city}&appid=${API.key}`)
+  .then(res => res.json())
+  .then(data =>
+    {
+      console.log(data)
+      setresult(data)
+    })
 }
- useEffect(() => {
+useEffect(() => {
   fetch(`${API.url}?q=karachi&appid=${API.key}`)
   .then(res => res.json())
   .then(data =>
@@ -23,9 +34,14 @@ const API ={
     <div className='App'>
       Weather
       <h1>{result.main && result.main.temp}<sup>o</sup>F</h1>
-      <h1><WiCloud />{result.main && result.main.humidity}</h1>
+      <h1>{result.main && result.main.humidity}</h1>
+      <div>
+        <input type='text' name='city' onChange={(e)=> setcity(e.target.value)}></input>
+        <button type='button' onClick={handleSearch}>Search</button>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
